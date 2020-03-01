08e1f45d1f84081a62911c7a96bcf486dc2181b9
b3lf <b3lf0817@gmail.com>
2020-03-01 12:26:34 +0900

    remove show() change some comments

--- Y_=_sin(X)_and_cos(X).py
+++ Y_=_sin(X)_and_cos(X).py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 描画範囲の指定
-# x = np.arange(x軸の最小値, x軸の最大値, 刻み)
+# set the range
 x = np.arange(-12, 12, 0.1)
 
-# 計算式
+# calculate
 y1 = np.sin(x)
 y2 = np.cos(x)
 
-# 横軸の変数。縦軸の変数。
+# set horizontal/vertical axises
 plt.plot(x, y1, label="sin") # y1グラフに、ラベル「sin」付与。
 plt.plot(x, y2, linestyle="--", label="cos") # y2グラフに、ラベル「cos」付与。linestyleで線のスタイルを指定。
 plt.xlabel("x-axis") 
@@ -17,13 +16,17 @@ plt.ylabel("y-axis")
 plt.title('Title: sin & cos')
 plt.legend()
 
-# 補助線
+# set auxiliary line
 plt.hlines(0, -12, 12, colors='r', linewidths=2)
 plt.vlines(0, -2, 2, colors='r', linewidths=2)
 
-# 描画実行
+# draw the graph
 plt.xlim([-2,2])
 plt.ylim([-2,2])
 plt.grid(True)
+
+# save as a png file
 plt.savefig(r'.\plot.png')
-plt.show()
+
+# show the graph
+# plt.show()
